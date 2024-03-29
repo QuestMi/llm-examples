b6597a6ff20c1688901383ec40ae0ac8264516bb
QuestMi <xiaoqinglin2018@gmail.com>
2024-03-29 03:49:06 +0000

    [refact]删除多余代码

--- Chatbot.py
+++ Chatbot.py
@@ -7,17 +7,17 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
+st.title("💬 Chatbot机器人")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "请问有什么可以帮助您?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.info("请输入您的OpenAI API key再继续.")
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
